9e4ec9b79517977f66527d7d01449640800df8f0
ssstefan <stefan93ss@hotmail.com>
2016-10-26 21:25:36 +0200

    init

---
+++ src/main/java/rs/fon/artifact/Hello.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rs.fon.artifact;
+
+/**
+ *
+ * @author stefan
+ */
+public class Hello {
+    
+    public void hello(String name) {
+        System.out.println("Hello " + name);
+    }
+    
+}
